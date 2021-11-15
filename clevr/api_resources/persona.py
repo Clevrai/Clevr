b95364239da990bf7771d7e6e49cc338e4ff6c16
Clevr <72430968+Clevrai@users.noreply.github.com>
2021-11-14 19:26:48 -0600

    Create persona.py

---
+++ clevr/api_resources/persona.py
@@ -0,0 +1,15 @@
+import json
+import requests
+
+"""
+Please request access at https://beta.clevr-ai.com/persona/apply. SDK File will be sent to applicants that are accepted.
+"""
+
+class Persona():
+
+    def query():
+      return "Coming soon! :)"
+    
+    
+    def list_models():
+      return "Coming soon! :D"
